f3f6e4f9906dbf01e287b7cb8789cdbb2437a2e1
SalmanDoria <salmandoria@gmail.com>
2022-11-06 18:27:18 -0500

    commit tree ae766ac6edd4e5f8757a0f1f2ec36c5f65cec924

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num){
+  let result
+  if (num <= 400){
+  result = "This one is on me!";
+   }
+
+  else if (401<= num && num < 2000){
+  result = "That will be twenty bucks.";
+  }
+  else if (2000 <= num && num < 2500){
+    result = "I will gladly take your thirty bucks."
+   
+  }
+  else if (num > 2500) {
+    result = "No can do."
+  }
+   
+  return result
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+scuberGreetingForFeet(200)
+
+function ternaryCheckCity(city){
+  let message
+  city === "NYC" ? message = "Ok, sounds good." : message = "No go.";
+return message
+}
+// what does italicized colon mean? 
+ternaryCheckCity(NYC)
+
+function switchOnCharmFromTip(tip){
+switch (tip) {
+  case "generous" :
+    return "Thank you so much.";
+  case "not as generous" :
+    return "Thank you.";
+  default :
+  return "Bye."
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  
 }
